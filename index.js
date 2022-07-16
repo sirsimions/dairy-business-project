8fff25ab499d100e6ef6d02534fc8c490adac76f
Simion Okola <simion.okola@moringaschool.com>
2022-07-16 23:32:10 +0300

    This commit will create the forms

---
+++ index.js
@@ -0,0 +1,39 @@
+var butt = document.getElementById('but1');
+butt.addEventListener("onclick", tabRows);
+
+var months =[];
+var weeks =[];
+var days =[];
+var milkLit =[];
+var sheds =[];
+var row = 1;
+var r=0;
+
+function tabRows(){
+    var tableDat = document.getElementById("tabl"); //getting the table
+ 
+    var rows = tableDat.insertRow(row); //inserting a row (containing headers)
+
+    months[r] = document.getElementById("month").value;
+    weeks[r] = document.getElementById("week").value;
+    days[r] = document.getElementById("day").value;
+    sheds[r] = document.getElementById("shed").value;
+    milkLit[r] = document.getElementById("milk").value;
+
+        var cel1 = rows.insertCell(0);
+        var cel2 = rows.insertCell(1);
+        var cel3 = rows.insertCell(2);
+        var cel4 = rows.insertCell(3);
+        var cel5 = rows.insertCell(4);
+        
+        cel1.innerHTML = months[r];
+        cel2.innerHTML = weeks[r];
+        cel3.innerHTML = days[r];
+        cel4.innerHTML = sheds[r];
+        cel5.innerHTML = milkLit[r];
+        
+        row++;
+        r++;
+    }
+
+
